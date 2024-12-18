6aae7341f4f7b863bab7db8bf6f166253242cc56
PC <PC@host.docker.internal>
2024-12-18 17:29:03 +0100

    app init

---
+++ src/main/java/dev/mds/interview/mdstechnicaltask/MdsTechnicalTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.mds.interview.mdstechnicaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsTechnicalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdsTechnicalTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mds/interview/mdstechnicaltask/MdsTechnicalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mds.interview.mdstechnicaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsTechnicalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
